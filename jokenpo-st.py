391f2bd30a1720555754bb8a72e04c8974ae6f6a
Bia Nami <125317931+bianami@users.noreply.github.com>
2023-08-08 11:36:14 -0300

    Update jokenpo-st.py

--- jokenpo-st.py
+++ jokenpo-st.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import random
 from PIL import Image
@@ -10,30 +11,46 @@ def main():
     paper_image = Image.open("paper.png")
     scissors_image = Image.open("scissors.png")
     
-    # Exibe as imagens para que o usuário possa clicar nelas
-    user_choice = st.image([rock_image, paper_image, scissors_image], caption=["Pedra", "Papel", "Tesoura"], width=150, use_column_width=True)
+    # Cria duas colunas para exibir as imagens lado a lado
+    col1, col2, col3 = st.beta_columns(3)
+    
+    # Exibe as imagens nas colunas
+    with col1:
+        st.image(rock_image, caption="Pedra", width=100)
+    with col2:
+        st.image(paper_image, caption="Papel", width=100)
+    with col3:
+        st.image(scissors_image, caption="Tesoura", width=100)
+    
+    # Gera uma escolha aleatória para o computador
+    computer_choice = random.choice(["Pedra", "Papel", "Tesoura"])
+    
+    # Exibe o resultado do jogo
+    result = play_game(computer_choice)
+    st.write(result)
+
+def play_game(computer_choice):
+    user_choice = None
+    
+    # Espera até que o usuário faça uma escolha
+    with st.form("Escolha sua jogada"):
+        with st.beta_container():
+            user_choice = st.selectbox("Escolha sua jogada:", ["Pedra", "Papel", "Tesoura"])
+            st.write(f"Você escolheu: {user_choice}")
+            
+            # Botão para submeter a escolha
+            submit_button = st.form_submit_button(label="Jogar")
     
     if user_choice:
-        user_choice_caption = user_choice.caption
-        st.write(f"Você escolheu: {user_choice_caption}")
-        
-        # Gera uma escolha aleatória para o computador
-        computer_choice = random.choice(["Pedra", "Papel", "Tesoura"])
-        st.write(f"Computador escolheu: {computer_choice}")
-        
         # Determina o resultado do jogo
-        result = determine_winner(user_choice_caption, computer_choice)
-        st.write(result)
-
-def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return "Empate!"
-    elif (user_choice == "Pedra" and computer_choice == "Tesoura") or \
-         (user_choice == "Papel" and computer_choice == "Pedra") or \
-         (user_choice == "Tesoura" and computer_choice == "Papel"):
-        return "Você ganhou!"
-    else:
-        return "Você perdeu!"
+        if user_choice == computer_choice:
+            return "Empate!"
+        elif (user_choice == "Pedra" and computer_choice == "Tesoura") or \
+             (user_choice == "Papel" and computer_choice == "Pedra") or \
+             (user_choice == "Tesoura" and computer_choice == "Papel"):
+            return "Você ganhou!"
+        else:
+            return "Você perdeu!"
     
 if __name__ == "__main__":
     main()
