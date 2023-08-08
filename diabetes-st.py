613cf73ef51ca0648b3746d04b314340fdba5e16
Bia Nami <125317931+bianami@users.noreply.github.com>
2023-08-07 21:10:23 -0300

    Update diabetes-st.py

--- diabetes-st.py
+++ diabetes-st.py
@@ -23,36 +23,29 @@ x_train, x_text, y_train, y_test = train_test_split(x, y, test_size=0.2)
 #dados dos usuários com a função
 def get_user_date():
 
-pregnancies = st.sidebar.slider("Gravidez",0, 15, 1)
+  pregnancies = st.sidebar.slider("Gravidez",0, 15, 1)
+  glicose = st.sidebar.slider("Glicose", 0, 200, 110)
+  blood_pressure = st.sidebar.slider("Pressão Sanguínea", 0, 122, 72)
+  skin_thickness = st.sidebar.slider("Espessura da pele", 0, 99, 20)
+  insulin = st.sidebar.slider("Insulina", 0, 900, 30)
+  bni= st.sidebar.slider("Índice de massa corporal", 0.0, 70.0, 15.0)
+  dpf = st.sidebar.slider("Histórico familiar de diabetes", 0.0, 3.0, 0.0)
+  age = st.sidebar.slider ("Idade", 15, 100, 21)
 
-glicose = st.sidebar.slider("Glicose", 0, 200, 110)
+  #dicionário para receber informações
+  user_data = {'Gravidez': pregnancies,
+  'Glicose': glicose,
+  'Pressão Sanguínea': blood_pressure,
+  'Espessura da pele': skin_thickness,
+  'Insulina': insulin,
+  'Índice de massa corporal': bni,
+  'Histórico familiar de diabetes': dpf,
+  'Idade': age
+  }
+  
+  features = pd.DataFrame(user_data, index=[0])
 
-blood_pressure = st.sidebar.slider("Pressão Sanguínea", 0, 122, 72)
-
-skin_thickness = st.sidebar.slider("Espessura da pele", 0, 99, 20)
-
-insulin = st.sidebar.slider("Insulina", 0, 900, 30)
-
-bni= st.sidebar.slider("Índice de massa corporal", 0.0, 70.0, 15.0)
-
-dpf = st.sidebar.slider("Histórico familiar de diabetes", 0.0, 3.0, 0.0)
-
-age = st.sidebar.slider ("Idade", 15, 100, 21)
-
-#dicionário para receber informações
-user_data = {'Gravidez': pregnancies,
-'Glicose': glicose,
-'Pressão Sanguínea': blood_pressure,
-'Espessura da pele': skin_thickness,
-'Insulina': insulin,
-'Índice de massa corporal': bni,
-'Histórico familiar de diabetes': dpf,
-'Idade': age
-}
-
-features = pd.DataFrame(user_data, index=[0])
-
-return features
+  return features
 
 user_input_variables = get_user_date()
 
@@ -77,6 +70,4 @@ prediction = dtc.predict(user_input_variables)
 st.subheader('Previsão:')
 
 st.write(prediction)
-
-
 
