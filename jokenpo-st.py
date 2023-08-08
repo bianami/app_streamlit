74a026580f113e309fa9096fe8ba3bd963a18771
Bia Nami <125317931+bianami@users.noreply.github.com>
2023-08-08 11:37:27 -0300

    Update jokenpo-st.py

--- jokenpo-st.py
+++ jokenpo-st.py
@@ -1,56 +1,32 @@
-
 import streamlit as st
 import random
-from PIL import Image
 
 def main():
     st.title("Jogo de Pedra, Papel e Tesoura")
     
-    # Faz o upload das imagens
-    rock_image = Image.open("rock.png")
-    paper_image = Image.open("paper.png")
-    scissors_image = Image.open("scissors.png")
+    options = ["pedra", "papel", "tesoura"]
     
-    # Cria duas colunas para exibir as imagens lado a lado
-    col1, col2, col3 = st.beta_columns(3)
-    
-    # Exibe as imagens nas colunas
-    with col1:
-        st.image(rock_image, caption="Pedra", width=100)
-    with col2:
-        st.image(paper_image, caption="Papel", width=100)
-    with col3:
-        st.image(scissors_image, caption="Tesoura", width=100)
+    # Exibe as opções de escolha para o usuário
+    user_choice = st.selectbox("Escolha sua jogada:", options)
+    st.write(f"Você escolheu: {user_choice}")
     
     # Gera uma escolha aleatória para o computador
-    computer_choice = random.choice(["Pedra", "Papel", "Tesoura"])
+    computer_choice = random.choice(options)
+    st.write(f"Computador escolheu: {computer_choice}")
     
-    # Exibe o resultado do jogo
-    result = play_game(computer_choice)
+    # Determina o resultado do jogo
+    result = determine_winner(user_choice, computer_choice)
     st.write(result)
 
-def play_game(computer_choice):
-    user_choice = None
-    
-    # Espera até que o usuário faça uma escolha
-    with st.form("Escolha sua jogada"):
-        with st.beta_container():
-            user_choice = st.selectbox("Escolha sua jogada:", ["Pedra", "Papel", "Tesoura"])
-            st.write(f"Você escolheu: {user_choice}")
-            
-            # Botão para submeter a escolha
-            submit_button = st.form_submit_button(label="Jogar")
-    
-    if user_choice:
-        # Determina o resultado do jogo
-        if user_choice == computer_choice:
-            return "Empate!"
-        elif (user_choice == "Pedra" and computer_choice == "Tesoura") or \
-             (user_choice == "Papel" and computer_choice == "Pedra") or \
-             (user_choice == "Tesoura" and computer_choice == "Papel"):
-            return "Você ganhou!"
-        else:
-            return "Você perdeu!"
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "Empate!"
+    elif (user_choice == "pedra" and computer_choice == "tesoura") or \
+         (user_choice == "papel" and computer_choice == "pedra") or \
+         (user_choice == "tesoura" and computer_choice == "papel"):
+        return "Você ganhou!"
+    else:
+        return "Você perdeu!"
     
 if __name__ == "__main__":
     main()
