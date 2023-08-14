3ce4dcdc124fdec76caa631a4e25b6aceb9a6526
Bia Nami <125317931+bianami@users.noreply.github.com>
2023-08-14 14:26:57 -0300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,50 +1,42 @@
 import streamlit as st
 
-# Função para a página 1
-def page1():
-    st.title("Página 1 - Pergunta 1")
-    st.write("Qual é a capital da França?")
-    options = ["Paris", "Londres", "Berlim"]
-    answer = st.radio("Escolha a resposta:", options)
-    return answer
+# Defina as perguntas e respostas
+questions = [
+    {
+        "question": "Qual é a capital da França?",
+        "options": ["Paris", "Londres", "Berlim"],
+        "correct_answer": "Paris"
+    },
+    {
+        "question": "Qual é o maior planeta do sistema solar?",
+        "options": ["Terra", "Júpiter", "Marte"],
+        "correct_answer": "Júpiter"
+    },
+    {
+        "question": "Qual é a cor do céu em um dia claro?",
+        "options": ["Verde", "Azul", "Vermelho"],
+        "correct_answer": "Azul"
+    }
+]
 
-# Função para a página 2
-def page2():
-    st.title("Página 2 - Pergunta 2")
-    st.write("Qual é o maior planeta do sistema solar?")
-    options = ["Terra", "Júpiter", "Marte"]
-    answer = st.radio("Escolha a resposta:", options)
-    return answer
-
-# Função para a página 3
-def page3():
-    st.title("Página 3 - Pergunta 3")
-    st.write("Qual é a cor do céu em um dia claro?")
-    options = ["Verde", "Azul", "Vermelho"]
-    answer = st.radio("Escolha a resposta:", options)
-    return answer
-
-# Função principal
 def main():
-    st.sidebar.title("Navegação")
-    page_options = ["Página 1", "Página 2", "Página 3"]
-    selected_page = st.sidebar.radio("Selecione a página:", page_options)
+    st.title("Jogo de Perguntas e Respostas")
+    total_questions = len(questions)
+    score = 0
     
-    if selected_page == "Página 1":
-        answer1 = page1()
-        st.sidebar.write("Resposta Pergunta 1:", answer1)
-    elif selected_page == "Página 2":
-        answer2 = page2()
-        st.sidebar.write("Resposta Pergunta 2:", answer2)
-    elif selected_page == "Página 3":
-        answer3 = page3()
-        st.sidebar.write("Resposta Pergunta 3:", answer3)
+    for i, q in enumerate(questions):
+        st.write(f"**Pergunta {i+1}:** {q['question']}")
+        answer = st.radio("Escolha a resposta:", q['options'])
+        if answer == q['correct_answer']:
+            score += 1
+            st.write("Resposta correta!")
+        else:
+            st.write("Resposta incorreta.")
+        
+        if i < total_questions - 1:
+            st.button("Próxima Pergunta")
     
-    if st.sidebar.button("Mostrar Resultado"):
-        st.write("Resultado Final:")
-        st.write("Resposta Pergunta 1:", answer1)
-        st.write("Resposta Pergunta 2:", answer2)
-        st.write("Resposta Pergunta 3:", answer3)
+    st.write(f"Seu placar final: {score}/{total_questions}")
 
 if __name__ == "__main__":
     main()
